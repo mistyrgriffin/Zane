c3b245296e65b8d4bb348a36774d9297011a46e6
Misty R Griffin <84652884+mistyrgriffin@users.noreply.github.com>
2023-01-23 08:55:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ let rederer = Matter.Render.create({
     options: {
         height: 600,
         width: 800
+        wireframes: false
     }
 })
 
-let ground = Matter.Bodies.rectangle(400, 200, 200, 50)
+let ground = Matter.Bodies.rectangle(600, 400, 200, 30),
+    isStatic: true,
+    render: {
+        fillStyle: 'brown'
+    }
+
 
 Matter.World.add(engine.world, ground)
 
