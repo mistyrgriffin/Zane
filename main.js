bddd33ccd40644d196cd9e0f2eaf633353e9225b
Misty R Griffin <84652884+mistyrgriffin@users.noreply.github.com>
2023-01-22 18:40:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ let engine = Matter.Engine.create()
 let rederer = Matter.Render.create({
     element: document.body,
     engine: engine,
+    options: {
+        height: 600
+        width: 800
+    }
 })
+
+let ground = Matter.Bodies.rectangle(400, 200, 200, 50)
+
+Matter.World.add(engine.world, ground)
 
 Matter.Render.run(rederer)
 Matter.Runner.run(engine)
